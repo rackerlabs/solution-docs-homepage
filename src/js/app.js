4edc14ef4fe6738f8e2e17975b3408d3142828da
Ravi <raviteja.valluri@rackspace.com>
2021-02-04 11:58:35 -0800

    CSS Fixes

--- src/js/app.js
+++ src/js/app.js
@@ -6,7 +6,10 @@ module.exports = moduleName;
 
 angular.module(moduleName, [
     require('angular-sanitize'),
+    require('./components/flex-height'),
     require('./components/global-sidebar'),
+    require('./components/collapsible-nav'),
+    require('./components/scroll-indicator'),
     require('./controllers/docs-home-services'),
     require('./controllers/docs-home-sidebar'),
     require('./components/sticky'),
