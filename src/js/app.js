661c6503eec8243627668b572b0a906001fad398
Ravi <raviteja.valluri@rackspace.com>
2021-02-04 10:55:57 -0800

    CSS Fixes

--- src/js/app.js
+++ src/js/app.js
@@ -6,8 +6,11 @@ module.exports = moduleName;
 
 angular.module(moduleName, [
     require('angular-sanitize'),
+    require('./components/collapsible-nav'),
+    require('./components/global-sidebar'),
     require('./controllers/docs-home-services'),
     require('./controllers/docs-home-sidebar'),
+    require('./components/sticky'),
     require('./services/filter'),
 ]);
 
