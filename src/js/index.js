bc8c40ffa8da155e1fe779b318ab6762d52cb4c4
Ravi <raviteja.valluri@rackspace.com>
2021-02-18 07:27:10 -0800

    Netlify Algolia Plugin

--- src/js/index.js
+++ src/js/index.js
@@ -3,10 +3,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './imports/App';
 ReactDOM.render(<App />, document.getElementById('search'));
-algoliasearchNetlify({
+console.log(algoliasearchNetlify({
     appId: ALGOLIA_APP_ID,
     apiKey: ALGOLIA_API_KEY,
     siteId: ALGOLIA_SITE_ID,
     branch: ALGOLIA_BRANCH,
     selector: 'div#search',
-});
+}));
