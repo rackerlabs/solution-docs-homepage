5331a6466e2879f2c98f9705ba88fa7e7ba1ac64
Ravi <raviteja.valluri@rackspace.com>
2021-02-18 07:43:30 -0800

    Netlify Algolia Plugin

--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,8 @@ var app = require('./app');
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './imports/App';
+import { algoliaCrawl, generateSitemap } from "algolia-crawl";
+
+await algoliaCrawl(); // Crawl all pages and sync index
+await generateSitemap("sitemap.xml"); // Generate a sitemap.xml file
 ReactDOM.render(<App />, document.getElementById('search'));
-console.log(algoliasearchNetlify({
-    appId: ALGOLIA_APP_ID,
-    apiKey: ALGOLIA_API_KEY,
-    siteId: ALGOLIA_SITE_ID,
-    branch: ALGOLIA_BRANCH,
-    selector: 'div#search',
-}));
