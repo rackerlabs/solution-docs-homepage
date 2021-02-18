c065b74d95728b44e094417d2acf50406aa55416
Ravi <raviteja.valluri@rackspace.com>
2021-02-18 10:39:02 -0800

    Netlify Algolia Plugin

--- webpack.config.js
+++ webpack.config.js
@@ -67,7 +67,7 @@ module.exports = {
       ALGOLIA_APP_ID: JSON.stringify(process.env.ALGOLIA_APP_ID),
       ALGOLIA_SEARCH_KEY: JSON.stringify(process.env.ALGOLIA_SEARCH_KEY),
       ALGOLIA_INDEX_NAME: JSON.stringify(process.env.ALGOLIA_INDEX_NAME),
-      ALGOLIA_INDEX_FILE: JSON.stringify(process.env.ALGOLIA_INDEX_FILE),
+      // ALGOLIA_INDEX_FILE: JSON.stringify(process.env.ALGOLIA_INDEX_FILE),
       ALGOLIA_ADMIN_KEY: JSON.stringify(process.env.ALGOLIA_ADMIN_KEY)
     })],
 };
