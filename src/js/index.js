ad2cb098806b60445aa85952937eb2a06a373635
Ravi <raviteja.valluri@rackspace.com>
2021-02-22 09:49:30 -0800

    New Redirects

--- src/js/index.js
+++ src/js/index.js
@@ -4,3 +4,11 @@ import ReactDOM from 'react-dom';
 import App from './imports/App';
 
 ReactDOM.render(<App />, document.getElementById('search'));
+
+algoliasearchNetlify({
+    appId: 'TYOYEKS0CV',
+    apiKey: '520ab86e5b53119be541d97e15bfe1c5',
+    siteId: '53f88b69-965d-4757-a1b9-fa99b18e92c7',
+    branch: 'master',
+    selector: 'div#search',
+});
