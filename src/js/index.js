27ed04fb2b8db5431a79cf4ecbeae745c7fa00c9
Ravi <raviteja.valluri@rackspace.com>
2021-02-22 12:45:15 -0800

    New Redirects

--- src/js/index.js
+++ src/js/index.js
@@ -9,5 +9,6 @@ algoliasearchNetlify({
     appId: 'TYOYEKS0CV',
     apiKey: '520ab86e5b53119be541d97e15bfe1c5',
     siteId: '53f88b69-965d-4757-a1b9-fa99b18e92c7',
-    branch: 'master'
+    branch: 'master',
+    selector: 'div#search',
 });
