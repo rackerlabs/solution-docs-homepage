eb7d6d41949668957b1015bc4c24a34814c53062
Ravi <raviteja.valluri@rackspace.com>
2021-02-22 13:10:01 -0800

    Algolia netlify plugin

--- src/js/index.js
+++ src/js/index.js
@@ -4,11 +4,3 @@ var app = require('./app');
 // import App from './imports/App';
 
 // ReactDOM.render(<App />, document.getElementById('search'));
-
-algoliasearchNetlify({
-    appId: 'TYOYEKS0CV',
-    apiKey: '9216222240a120ca2d6c266ebd945046',
-    siteId: '53f88b69-965d-4757-a1b9-fa99b18e92c7',
-    branch: 'master',
-    selector: 'div#search',
-  });
