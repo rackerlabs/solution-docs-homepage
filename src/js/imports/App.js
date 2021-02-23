658a91bb07a0697840f5f67cb213b880ab7b585f
Ravi <raviteja.valluri@rackspace.com>
2021-02-23 14:15:42 -0800

    algolia netlify crawler custom search

--- src/js/imports/App.js
+++ src/js/imports/App.js
@@ -4,7 +4,6 @@ import { InstantSearch, SearchBox, Configure } from 'react-instantsearch-dom';
 import InfiniteHits from './InfiniteHits';
 import { connectStateResults } from "react-instantsearch/connectors"
 
-const ALGOLIA_INDEX_NAME = 'netlify_22f90b68-b04d-411c-a401-a4e19170c6ae_master_all';
 const ALGOLIA_SEARCH_KEY = '12aeb45caf8aa0724cf852a04e6c0646';
 const ALGOLIA_APP_ID = 'A05K9F5RGB';
 const algoliaClient = algoliasearch(
@@ -24,6 +23,7 @@ const Results = connectStateResults(
   }
 );
 const searchClient = {
+  
     search(requests) {
         if (
             requests.every(({ params }) => !params.query.trim()
@@ -42,7 +42,9 @@ const searchClient = {
 };
 
 class App extends Component {
+  
   render() {
+    const ALGOLIA_INDEX_NAME = 'netlify_22f90b68-b04d-411c-a401-a4e19170c6ae_master_all';
     return (
       <div className="ais-InstantSearch">
         <InstantSearch indexName={ALGOLIA_INDEX_NAME} searchClient={searchClient}>
