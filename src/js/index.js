6510d1445c2729ee738f49cfe105c3156337dbda
Ravi <raviteja.valluri@rackspace.com>
2021-02-23 11:15:45 -0800

    remove algolia netlify crawler

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 var app = require('./app');
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './imports/App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './imports/App';
 
-// ReactDOM.render(<App />, document.getElementById('search'));
+ReactDOM.render(<App />, document.getElementById('search'));
