c5b2ffe3d3123b2bf4624541137b592a4d0f1e26
Ravi <raviteja.valluri@rackspace.com>
2021-02-23 13:32:24 -0800

    algolia netlify crawler

--- src/js/index.js
+++ src/js/index.js
@@ -3,4 +3,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './imports/App';
 
+algoliasearchNetlify({
+    appId: 'A05K9F5RGB',
+    apiKey: '12aeb45caf8aa0724cf852a04e6c0646',
+    siteId: '22f90b68-b04d-411c-a401-a4e19170c6ae',
+    branch: 'master',
+    selector: 'div#search',
+});
+
 ReactDOM.render(<App />, document.getElementById('search'));
