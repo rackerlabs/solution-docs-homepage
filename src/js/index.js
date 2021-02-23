26587c24ce77dfe2b4986720bc35e64b71615183
Ravi <raviteja.valluri@rackspace.com>
2021-02-23 14:18:15 -0800

    algolia netlify crawler custom search

--- src/js/index.js
+++ src/js/index.js
@@ -8,7 +8,6 @@ algoliasearchNetlify({
     apiKey: '12aeb45caf8aa0724cf852a04e6c0646',
     siteId: '22f90b68-b04d-411c-a401-a4e19170c6ae',
     branch: 'master',
-    selector: 'div#crawler-search',
 });
 
 ReactDOM.render(<App />, document.getElementById('search'));
