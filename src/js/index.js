4765c18ecc9bcaf58da9026cb5bce685c5b23393
Ravi <raviteja.valluri@rackspace.com>
2021-02-23 08:51:03 -0800

    Algolia Netlify Crawler Plugin

--- src/js/index.js
+++ src/js/index.js
@@ -4,11 +4,3 @@ var app = require('./app');
 // import App from './imports/App';
 
 // ReactDOM.render(<App />, document.getElementById('search'));
-
-algoliasearchNetlify({
-    appId: '0TMCRRQBXZ',
-    apiKey: '0d45d48251386776c432ab7f164df7b2',
-    siteId: '53f88b69-965d-4757-a1b9-fa99b18e92c7',
-    branch: 'master',
-    selector: 'div#search',
-});
